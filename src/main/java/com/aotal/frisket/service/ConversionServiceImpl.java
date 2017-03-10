e556d298cd6ae5bb17cf20646042ae3e35e23b0e
allanks <allan.koster-smith@aotal.com>
2017-03-10 13:25:56 +1300

    Adding in wait for processes to ensure finishing tasks

--- src/main/java/com/aotal/frisket/service/ConversionServiceImpl.java
+++ src/main/java/com/aotal/frisket/service/ConversionServiceImpl.java
@@ -47,7 +47,11 @@ public class ConversionServiceImpl implements ConversionService {
                     case "text/html":
                     case "text/htm":
                         ProcessBuilder pb = new ProcessBuilder("wkhtmltopdf", "--quiet", path.toString(), to.resolve(path.getFileName()).toString());
-                        pb.start();
+                        try {
+                            pb.start().waitFor();
+                        } catch (InterruptedException e) {
+                            // Deliberaterly left blank
+                        }
                         break;
                     default:
                         files.add(path.toString());
@@ -60,21 +64,27 @@ public class ConversionServiceImpl implements ConversionService {
         ProcessBuilder dos2unix = new ProcessBuilder(Stream.concat(Stream.of("dos2unix", "--quiet"), files.stream()).collect(Collectors.toList()));
         Span dos2unixSp = tracer.createSpan("Dos2Unix converting", span);
         try {
-            dos2unix.start();
+            dos2unix.start().waitFor();
+        } catch (InterruptedException e) {
+            // Deliberaterly left blank
         } finally {
             tracer.close(dos2unixSp);
         }
         ProcessBuilder libre = new ProcessBuilder(Stream.concat(Stream.of("lowriter", "--invisible", "--convert-to", "pdf:writer_pdf_Export:UTF8", "--outdir", "processed"), files.stream()).collect(Collectors.toList()));
         Span libreSp = tracer.createSpan("Libreoffice Converting", span);
         try {
-            libre.start();
+            libre.start().waitFor();
+        } catch (InterruptedException e) {
+            // Deliberaterly left blank
         } finally {
             tracer.close(libreSp);
         }
         ProcessBuilder gs = new ProcessBuilder(Stream.concat(Stream.of("gs", "-dBATCH", "-dNOPAUSE", "-dPDFFitPage", "-q", "-sOwnerPassword=reallylongandsecurepassword", "-sDEVICE=pdfwrite", "-sOutputFile=" + to.resolve(filename).toString() + ".pdf"), Files.list(to).map(Path::toString)).collect(Collectors.toList()));
         Span gsSp = tracer.createSpan("Stitching", span);
         try {
-            gs.start();
+            gs.start().waitFor();
+        } catch (InterruptedException e) {
+            // Deliberaterly left blank
         } finally {
             tracer.close(gsSp);
         }
